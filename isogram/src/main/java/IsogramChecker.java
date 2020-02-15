c9efeb23e01c22ddf560970858a878408ad4fdba
Rahul Shah <rahulshah7@outlook.com>
2020-02-15 21:15:03 +0000

    Add psuedo code for streams approach

--- isogram/src/main/java/IsogramChecker.java
+++ isogram/src/main/java/IsogramChecker.java
@@ -8,3 +8,10 @@ class IsogramChecker {
         return false;
     }
 }
+
+/* Solve using streams */
+// convert phrase string to char array
+// convert char array to stream
+// filter out char < a and > z
+// is isogram if
+// #count stream and #count distinct stream
