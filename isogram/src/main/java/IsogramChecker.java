c4b0e2169f8d16575c08a8f733b4ff982d6500fe
Rahul Shah <rahulshah7@outlook.com>
2020-02-27 22:49:30 +0000

    Solve using replaceAll() and chars() String methods

--- isogram/src/main/java/IsogramChecker.java
+++ isogram/src/main/java/IsogramChecker.java
@@ -1,36 +1,8 @@
-import java.util.ArrayList;
-
 class IsogramChecker {
 
     boolean isIsogram(String phrase) {
-        if (phrase.isEmpty()) {
-            return true;
-        }
-
-        phrase = phrase.toLowerCase();
-        ArrayList<Character> letters = new ArrayList<>();
-        for (char character : phrase.toCharArray()) {
-            if (character >= 'a' && character <= 'z') {
-                if (letters.contains(character)) {
-                    return false;
-                }
-                letters.add(character);
-            }
-        }
-
-        return true;
+        phrase = phrase.toLowerCase().replaceAll("\\W", "");
+        return phrase.chars().distinct().count() == phrase.length();
     }
 
 }
-
-/* Solve using streams */
-// convert phrase string to char array
-// convert char array to stream
-// filter out char < a and > z
-// is isogram if
-// #count stream and #count distinct stream
-
-/* Solve using Hash maps */
-// create Hash map. Keys are chars a to z, values = 0;
-// loop over phrase chars a to z and increment Hash map value
-// return false if hashmap value > 1
