d29f8be4559dc9a63653b849a0d590c3ab8399df
Rahul Shah <rahulshah7@outlook.com>
2020-03-01 20:26:45 +0000

    Remove Twofer duplication per mentor feedback

--- two-fer/src/main/java/Twofer.java
+++ two-fer/src/main/java/Twofer.java
@@ -1,8 +1,8 @@
 class Twofer {
     String twofer(String name) {
         if (name == null)
-            return "One for you, one for me.";
-        else
-            return String.format("One for %s, one for me.", name);
+            name = "you";
+
+        return String.format("One for %s, one for me.", name);
     }
 }
