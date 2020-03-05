320e3a826b04a70e89808f147b024b87a8a9513d
Rahul Shah <rahulshah7@outlook.com>
2020-03-05 11:28:54 +0000

    Get hamming distance for valid inputs

--- hamming/src/main/java/Hamming.java
+++ hamming/src/main/java/Hamming.java
@@ -1,11 +1,19 @@
 class Hamming {
+    private String leftStand;
+    private String rightStand;
+    private int hammingDistance = 0;
 
     Hamming(String leftStrand, String rightStrand) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.leftStand = leftStrand;
+        this.rightStand = rightStrand;
     }
 
     int getHammingDistance() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        for (int i = 0; i < rightStand.length(); i++) {
+            if (leftStand.charAt(i) != rightStand.charAt(i)) {
+                this.hammingDistance++;
+            }
+        }
+        return this.hammingDistance;
     }
-
 }
--- hamming/src/test/java/HammingTest.java
+++ hamming/src/test/java/HammingTest.java
@@ -16,25 +16,21 @@ public class HammingTest {
         assertEquals(0, new Hamming("", "").getHammingDistance());
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testNoDistanceBetweenShortIdenticalStrands() {
         assertEquals(0, new Hamming("A", "A").getHammingDistance());
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testCompleteDistanceInSingleLetterDifferentStrands() {
         assertEquals(1, new Hamming("G", "T").getHammingDistance());
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testDistanceInLongIdenticalStrands() {
         assertEquals(0, new Hamming("GGACTGAAATCTG", "GGACTGAAATCTG").getHammingDistance());
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testDistanceInLongDifferentStrands() {
         assertEquals(9, new Hamming("GGACGGATTCTG", "AGGACGGATTCT").getHammingDistance());
