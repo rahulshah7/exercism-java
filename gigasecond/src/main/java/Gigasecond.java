d83abde78593c356b9648ac81628eddc39ae582c
Rahul Shah <rahulshah7@outlook.com>
2020-03-08 14:57:48 +0000

    Solve gigasecond - iteration 2 using mentor feedback

--- gigasecond/src/main/java/Gigasecond.java
+++ gigasecond/src/main/java/Gigasecond.java
@@ -2,19 +2,27 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-class Gigasecond {
+public class Gigasecond {
     private static final Duration GIGA_SECOND = Duration.ofSeconds(1_000_000_000);
     private LocalDateTime moment;
 
-    Gigasecond(LocalDate moment) {
+    public Gigasecond(LocalDate moment) {
+        validateMomentParameterNotNull(moment);
         this.moment = moment.atStartOfDay();
     }
 
-    Gigasecond(LocalDateTime moment) {
+    public Gigasecond(LocalDateTime moment) {
+        validateMomentParameterNotNull(moment);
         this.moment = moment;
     }
 
-    LocalDateTime getDateTime() {
+    public LocalDateTime getDateTime() {
         return moment.plus(GIGA_SECOND);
+    }
+
+    private void validateMomentParameterNotNull(Object moment) {
+        if (moment == null) {
+            throw new IllegalArgumentException("Parameter \"moment\" cannot be null.");
+        }
     }
 }
