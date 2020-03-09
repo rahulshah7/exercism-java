2fee6e7716ca71ce8b955b0dc86a268174f1bd0e
Rahul Shah <rahulshah7@outlook.com>
2020-03-09 14:41:01 +0000

    Solve scrabble-score - iteration 1

--- scrabble-score/src/main/java/Scrabble.java
+++ scrabble-score/src/main/java/Scrabble.java
@@ -1,11 +1,29 @@
-class Scrabble {
+import java.util.HashMap;
 
-    Scrabble(String word) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+public class Scrabble {
+    private static final HashMap<Character, Integer> letterScores = initLetterScores();
+    private final String word;
+    private int score;
+
+    public Scrabble(String word) {
+       this.word = word.toUpperCase();
     }
 
-    int getScore() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    public int getScore() {
+        for (int i = 0; i < this.word.length(); i++) {
+            score += letterScores.get(this.word.charAt(i));
+        }
+        return score;
     }
 
+    private static HashMap<Character, Integer> initLetterScores() {
+        final char[] letters = {'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T', 'D', 'G', 'B', 'C', 'M', 'P', 'F', 'H', 'V', 'W', 'Y', 'K', 'J', 'X', 'Q', 'Z'};
+        final int[] scores = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 8, 8, 10, 10};
+        final HashMap<Character, Integer> letterScores = new HashMap<>();
+
+        for(int i= 0; i < letters.length; i++){
+            letterScores.put(letters[i], scores[i]);
+        }
+        return letterScores;
+    }
 }
