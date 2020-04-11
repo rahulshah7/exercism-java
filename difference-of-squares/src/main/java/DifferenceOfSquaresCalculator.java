7ce51ae87f65abfdafcfe6d8d3b9c34224ece866
Rahul Shah <rahulshah7@outlook.com>
2020-04-11 21:57:51 +0100

    Solve difference-of-squares - iteration 2

--- difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
+++ difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
@@ -1,18 +1,12 @@
 class DifferenceOfSquaresCalculator {
 
     int computeSquareOfSumTo(int input) {
-        int result = 0;
-        for (int i = 1; i <= input; i++) {
-            result += i;
-        }
+        int result = input * (input + 1) / 2;
         return result * result;
     }
 
     int computeSumOfSquaresTo(int input) {
-        int result = 0;
-        for (int i = 1; i <= input; i++) {
-            result += i * i;
-        }
+        int result = input * (input + 1) * (2 * input + 1) / 6;
         return result;
     }
 
