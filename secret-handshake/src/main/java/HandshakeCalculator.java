2f9c9d255d3bd9c296ab4a2b139088b25faac8a0
Rahul Shah <rahulshah7@outlook.com>
2020-04-27 12:29:49 +0100

    Refactor solution for secret-handshake - Iteration 1

--- secret-handshake/src/main/java/HandshakeCalculator.java
+++ secret-handshake/src/main/java/HandshakeCalculator.java
@@ -5,41 +5,33 @@ import java.util.List;
 class HandshakeCalculator {
 
     List<Signal> calculateHandshake(int number) {
-        List<Signal> handshake = new ArrayList<>();
 
+        List<Signal> handshake = new ArrayList<>();
         number = Integer.parseInt(Integer.toBinaryString(number));
 
         while (number > 0) {
-            boolean reverseHandshake = false;
-            while (number >= 10000) {
-                number = number - 10000;
-                reverseHandshake = true;
-            }
 
-            while (number >= 1000) {
-                handshake.add(0, Signal.JUMP);
-                number = number - 1000;
-            }
+            boolean reverseHandshake = number >= 10000;
 
-            while (number >= 100) {
-                handshake.add(0, Signal.CLOSE_YOUR_EYES);
-                number = number - 100;
-            }
+            number = addSignalToHandshake(handshake, number, null, 10000);
+            number = addSignalToHandshake(handshake, number, Signal.JUMP, 1000);
+            number = addSignalToHandshake(handshake, number, Signal.CLOSE_YOUR_EYES, 100);
+            number = addSignalToHandshake(handshake, number, Signal.DOUBLE_BLINK, 10);
+            number = addSignalToHandshake(handshake, number, Signal.WINK, 1);
 
-            while (number >= 10) {
-                handshake.add(0, Signal.DOUBLE_BLINK);
-                number = number - 10;
-            }
-
-            while (number >= 1) {
-                handshake.add(0, Signal.WINK);
-                number = number - 1;
-            }
-
-            if (reverseHandshake)
+            if (reverseHandshake) {
                 Collections.reverse(handshake);
+            }
         }
-
         return handshake;
+    }
+
+    private int addSignalToHandshake(List<Signal> handshake, int number, Signal signal, int signalValue) {
+        while (number >= signalValue) {
+            if (signal != null)
+                handshake.add(0, signal);
+            number -= signalValue;
+        }
+        return number;
     }
 }
